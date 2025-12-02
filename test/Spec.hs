c59b08076fa05232e96a872feef2f510cffab3b9
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2025-12-02 22:45:27 +0900

    update specs

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,1 +1,8 @@
-{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+import Test.Hspec
+import qualified Day01Spec
+import qualified Day02Spec
+
+main :: IO ()
+main = hspec $ do
+    describe "Day01" Day01Spec.spec
+    describe "Day02" Day02Spec.spec
