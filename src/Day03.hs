c15ed637e0146ddb7e65de647ffe92733b37bcb8
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2025-12-03 20:44:37 +0900

    solver day 3

--- src/Day03.hs
+++ src/Day03.hs
@@ -1,7 +1,22 @@
 module Day03 (part1, part2) where
 
 part1 :: String -> Int
-part1 _ = 0
+part1 input = sum $ map solveBank (lines input)
+
+solveBank :: String -> Int
+solveBank s = 
+    let -- find max digit excluding last char (since y must follow x)
+        candidatesX = init s
+        maxX = maximum candidatesX
+        
+        -- find first occurrence of maxX to maximize remaining suffix for y
+        (_, matchAndAfter) = break (== maxX) s
+        
+        -- find max digit in the suffix after maxX
+        candidatesY = tail matchAndAfter
+        maxY = maximum candidatesY
+        
+    in read [maxX, maxY]
 
 part2 :: String -> Int
 part2 _ = 0
