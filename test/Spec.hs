cf828619cea90829e7aff6a503b7c7bbc402d096
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2025-12-03 20:57:07 +0900

    stub day 4

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,9 +2,11 @@ import Test.Hspec
 import qualified Day01Spec
 import qualified Day02Spec
 import qualified Day03Spec
+import qualified Day04Spec
 
 main :: IO ()
 main = hspec $ do
     describe "Day01" Day01Spec.spec
     describe "Day02" Day02Spec.spec
     describe "Day03" Day03Spec.spec
+    describe "Day04" Day04Spec.spec
