0cf576460f21319e5e9daeb12fe2ca877bea468c
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2025-12-04 22:57:23 +0900

    day 4 solver

--- test/Day04Spec.hs
+++ test/Day04Spec.hs
@@ -3,11 +3,25 @@ module Day04Spec (spec) where
 import Test.Hspec
 import Day04
 
+exampleInput :: String
+exampleInput = unlines
+    [ "..@@.@@@@."
+    , "@@@.@.@.@@"
+    , "@@@@@.@.@@"
+    , "@.@@@@..@."
+    , "@@.@@@@.@@"
+    , ".@@@@@@@.@"
+    , ".@.@.@.@@@"
+    , "@.@@@.@@@@"
+    , ".@@@@@@@@."
+    , "@.@.@@@.@."
+    ]
+
 spec :: Spec
 spec = do
     describe "part1" $ do
-        it "works" $
-            part1 "" `shouldBe` 0
+        it "works with example" $
+            part1 exampleInput `shouldBe` 13
 
     describe "part2" $ do
         it "works" $
