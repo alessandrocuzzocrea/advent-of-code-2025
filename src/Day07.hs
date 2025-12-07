72a4c33c0b6f83c1da6eefbac61114afb8f6c865
Alessandro Cuzzocrea <alessandrocuzzocrea@users.noreply.github.com>
2025-12-07 18:48:22 +0900

    day 7 part 2 solution

--- src/Day07.hs
+++ src/Day07.hs
@@ -4,6 +4,7 @@ import Data.Set (Set)
 import qualified Data.Set as Set
 import Data.List (find)
 import Data.Maybe (mapMaybe)
+import qualified Data.Map as Map
 
 type Coord = (Int, Int)
 type Grid = [String]
@@ -49,4 +50,47 @@ simulate grid currentRow activeCols splitCount
             _   -> (Set.insert c accSet, accSplits) -- treat other chars as empty space
 
 part2 :: String -> Int
-part2 _ = 0
+part2 input = fromIntegral $ solvePart2 grid start
+  where
+    grid = lines input
+    start = findStart grid
+
+solvePart2 :: Grid -> Coord -> Integer
+solvePart2 grid (startRow, startCol) = simulateQuantum grid (startRow) (Map.singleton startCol 1) 0
+
+simulateQuantum :: Grid -> Int -> Map.Map Int Integer -> Integer -> Integer
+simulateQuantum grid currentRow activeCols finishedCount
+    | currentRow >= length grid = finishedCount + sum (Map.elems activeCols)
+    | Map.null activeCols = finishedCount
+    | otherwise = simulateQuantum grid (currentRow + 1) nextCols newFinishedCount
+  where
+    currentNbCols = length (grid !! currentRow)
+    rowStr = grid !! currentRow
+    
+    -- current row
+    (nextCols, addedFinished) = Map.foldrWithKey processCol (Map.empty, 0) activeCols
+    
+    newFinishedCount = finishedCount + addedFinished
+    
+    processCol :: Int -> Integer -> (Map.Map Int Integer, Integer) -> (Map.Map Int Integer, Integer)
+    processCol c count (accNext, accFin) =
+        if c < 0 || c >= currentNbCols
+        then (accNext, accFin + count) -- out of bounds
+        else
+            let cell = rowStr !! c
+            in case cell of
+                '^' -> 
+                    let leftC = c - 1
+                        rightC = c + 1
+                        -- check bounds for children
+                        (accNext1, accFin1) = addParticle leftC count currentNbCols (accNext, accFin)
+                        (accNext2, accFin2) = addParticle rightC count currentNbCols (accNext1, accFin1)
+                    in (accNext2, accFin2)
+                _ -> -- passes through (., S, etc)
+                    -- moves to same column in next row
+                    addParticle c count currentNbCols (accNext, accFin)
+
+    addParticle :: Int -> Integer -> Int -> (Map.Map Int Integer, Integer) -> (Map.Map Int Integer, Integer)
+    addParticle c count width (accMap, accFin)
+        | c < 0 || c >= width = (accMap, accFin + count)
+        | otherwise = (Map.insertWith (+) c count accMap, accFin)
